10aff0fadab985e0dd45a929b79b8b7e889ca3a2
John Petrov <work.petroff@gmail.com>
2018-06-27 00:35:02 +0300

    *

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
+console.log(__dirname);
+
+app.get('/', function(req, res) {
+	res.send('Works!');
+})
 
 app.listen(8081, '0.0.0.0', function() {
 	console.log('signup_demo express runs');
